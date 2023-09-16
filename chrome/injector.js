9dba287d7e529bce87d9443cf57f57c283e267b0
Riccardo Cozzi <57441529+riccardo-cozzi@users.noreply.github.com>
2023-09-17 01:34:12 +0200

    stylesheets

--- chrome/injector.js
+++ chrome/injector.js
@@ -2,12 +2,20 @@
  * Qui i tag della pagina sono visibili
  */
 function insert_into_textfield(id, value) {
+    console.log("Inserting " + value + " into " + id)
+    let notFoundItems = []
     try {
-        document.getElementById(id) = value
+        document.getElementById(id).value = value
     } catch (error) {
-        console.log("textfield with id " + id + " not found")
+        notFoundItems.push(id)
+    }
+
+    if (notFoundItems.length > 0) {
+        console.log("These items were not found: " + notFoundItems)
+    } else {
+        console.log("All items were filled")
     }
 }
 
-insert_into_textfield("name", "porcoddio")
-alert("modificato")
+insert_into_textfield("nome_form", "madonna")
+insert_into_textfield("email_form", "dio@cane.it")
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,21 +1,22 @@
-console.log("popup.js loaded")
 
-var btn = document.getElementById("btn")
-var nome = document.getElementById("nome")
-var cognome = document.getElementById("cognome")
-
-console.log("btn found")
-
-try {
-    chrome.tabs.query({ active: true, lastFocusedWindow: true })    
-    .then((result) => {
-        btn.addEventListener('click', function(){
-            chrome.scripting.executeScript({
-                target : {tabId : result[0].id},
-                files : [ "injector.js" ],
+function listen() {
+    try {
+        var btn = document.getElementById("btn")
+        console.log("btn found")
+        chrome.tabs.query({ active: true, lastFocusedWindow: true })    
+        .then((result) => {
+            btn.addEventListener('click', function(){
+                chrome.scripting.executeScript({
+                    target : {tabId : result[0].id},
+                    files : [ "injector.js" ],
+                })
             })
         })
-    })
-} catch (error) {
-    console.log("Btn not found on the popup")
+    } catch (error) {
+        console.log("Btn not found on the popup")
+    }
 }
+
+listen()
+
+
